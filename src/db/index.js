e446517d488b24fb9e369043e47a32832485d6e9
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-08 02:33:35 +0530

    fix - env bad credentials

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
         const connectionString = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MONGODB Connected !! DB HOST: ${connectionString.connection.host}`);
     } catch (error) {
-        console.log("MONGODB Connection error", error);
+        console.log("MONGODB Connection Failed", error);
         process.exit(1);
     }
 }
