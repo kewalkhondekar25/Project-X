7a1c1cf0ae4feb5bb8e176ac0cd0039792a0ed27
keal khondekar <kewalkhondekar25@gmail.com>
2024-02-12 19:59:59 +0530

    feat - added user & video modal

--- src/app.js
+++ src/app.js
@@ -11,5 +11,6 @@ app.use(cors({
 app.use(express.json({limit: "20kb"}));
 app.use(express.urlencoded({extended: true, limit: "20kb"}));
 app.use(express.static("public"));
+app.use(express.cookieParser());
 
 export {app};
